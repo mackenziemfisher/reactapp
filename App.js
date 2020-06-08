379ed0fb685b352e8249819129af11058f4a13f0
mackenziemfisher <66263985+mackenziemfisher@users.noreply.github.com>
2020-06-07 23:41:10 -0400

    Add files via upload

---
+++ App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { render } from '@testing-library/react';
+
+class App extends Component {
+  render() {
+   return  (
+       <div className="App"
+       <div
+
+
+
+
+
+
+
+
+
+
+
+
--- index.js
+++ index.js
@@ -3,9 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-<div>
-   The app is up and running;
-</div>
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
